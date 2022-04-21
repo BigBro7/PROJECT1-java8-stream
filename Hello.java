bfb1c370d5ccbb78d0007d0e000468bb1cef1591
Manjunath D S <savitrigolmanjunath@gmail.com>
2022-04-21 11:39:38 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,6 +2,7 @@ Class helloworld{
   public static void main(String args[]){
     System.out.println("hello world");
     System.out.println("this is github");
+    System.out.println("I connected github to visual studio code");
   }
 }
     
