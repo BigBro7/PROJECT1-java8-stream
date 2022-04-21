3b23aa7d633354f9deece0bdf0cb9dfd29cccc69
manjunath <manjunath@DESKTOP-OLNFQ9E>
2022-04-21 10:47:41 +0530

    Basic Eclipse Project

---
+++ githubProject/src/main/java/com/basic/code/ThisworldProgram.java
@@ -0,0 +1,11 @@
+package com.basic.code;
+
+public class ThisworldProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Is this world beautiful?? Yes it is.");
+
+	}
+
+}
